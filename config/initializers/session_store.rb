2d2fdd9cbbec331a598d139668e7f9eb2779d355
Chris <82674454+ChristianMRtz@users.noreply.github.com>
2022-03-25 22:26:40 -0500

    Updated session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_api_social_network_session', domain: 'localhost'
+Rails.application.config.session_store :cookie_store, key: '_api_social_network_session', domain: 'api-socialnetwork-cmr.herokuapp.com'
